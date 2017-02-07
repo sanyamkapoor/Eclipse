6a0d3e8d9dbcbdee7df082c03e581ececb268da9
Sanyam Kapoor <sanyam.kapoor202@gmail.com>
2017-02-07 16:26:30 +0530

    Second Change

--- FirstProject/src/Lesson1/sample.java
+++ FirstProject/src/Lesson1/sample.java
@@ -4,7 +4,7 @@ public class sample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello Java World Hey");
+System.out.println("Hello Java World Hello Can U Hear Me");
 	}
 
 }
