38ee82bca660b0e6260d83b7497c22a5b0894149
Sanyam Kapoor <sanyam.kapoor202@gmail.com>
2017-02-07 16:20:44 +0530

    Started

---
+++ FirstProject/src/Lesson1/MyClass.java
@@ -0,0 +1,17 @@
+package Lesson1;
+
+import java.util.Scanner;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan= new Scanner(System.in);
+		System.out.println("Enter The No");
+		int uin=scan.nextInt();
+		System.out.print("Entered No Is");
+		System.out.println(uin);
+
+	}
+
+}
